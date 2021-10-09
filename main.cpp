b780d71b0f7e7879da26ea4c9f9c5d69e13c6eec
Vladislav Nasevich <nasevich.vlasd.03@gmail.com>
2021-10-09 14:11:57 +0300

    -8th task done

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@
 
 using namespace std;
 
-double err_check() { // function that check type error
+double inizializeDouble() { // function that check type error
 	double temp_var; // inicialization of temporary variable 
 	while (!(cin >> temp_var) || temp_var < 0)
 	{
@@ -23,7 +23,7 @@ double err_check() { // function that check type error
 	return temp_var;
 }
 
-double err_check_for_negative() { // function that check type error
+double inicializeDoubleNegative() { // function that check type error
 	double temp_var; // inicialization of temporary variable 
 	while (!(cin >> temp_var))
 	{
@@ -34,7 +34,7 @@ double err_check_for_negative() { // function that check type error
 	return temp_var;
 }
 
-int err_check_int() { // function that check type error
+int inizializeInteger() { // function that check type error
 	int temp_var; // inicialization of temporary variable 
 	while (!(cin >> temp_var) || temp_var < 0)
 	{
@@ -45,7 +45,7 @@ int err_check_int() { // function that check type error
 	return temp_var;
 }
 
-int err_check_int_for_negative() { // function that check type error
+int inizializeIntegerNegative() { // function that check type error
 	int temp_var; // inicialization of temporary variable 
 	while (!(cin >> temp_var))
 	{
@@ -97,7 +97,7 @@ void f1() {
 	fin.close(); // closing file
 }
 
-int sign_definition(int a) { // for f2
+int defineSign(int a) { // for f2
 	if (a > 0)
 	{
 		return 1;
@@ -114,44 +114,44 @@ int sign_definition(int a) { // for f2
 
 void f2() {
 	cout << "Введите число, знак которого нужно определить:" << endl;
-	int x = err_check_int_for_negative();
-	cout << "Знак введёного числа соответствует числу " << sign_definition(x) << " в предоставленной системе." << endl;
+	int x = inizializeIntegerNegative();
+	cout << "Знак введёного числа соответствует числу " << defineSign(x) << " в предоставленной системе." << endl;
 }
 
-double S_rectangle(double a, double b) { // for f3
+double computeSquareRectangle(double a, double b) { // for f3
 	return a * b;
 }
 
-double S_triangle(double h, double x) { // for f3
+double computeSquareTriangle(double h, double x) { // for f3
 	return h * x / 2;
 }
 
-double S_round(double r) { // for f3
+double computeSquareCircle(double r) { // for f3
 	return M_PI * pow(r, 2);
 }
 
 void f3() {
 	cout << "Введите:\n1, если хотите посчитать S прямоугольника.\n2, если хотите посчитать S треугольника.\n3, если хотите посчитать S круга.\nВведённое значение:\n";
-	int chose = err_check_int(); // choosing figure
+	int chose = inizializeInteger(); // choosing figure
 	if (chose == 1)
 	{
 		cout << "Введите значения сторон прямоугольника:\n";
-		double a = err_check();
-		double b = err_check();
-		cout << "S прямоугольника = " << S_rectangle(a, b) << "." << endl;
+		double a = inizializeDouble();
+		double b = inizializeDouble();
+		cout << "S прямоугольника = " << computeSquareRectangle(a, b) << "." << endl;
 	}
 	else if (chose == 2)
 	{
 		cout << "Введите значения длин основания и проведённой к нему высоты:\n";
-		double h = err_check();
-		double x = err_check();
-		cout << "S треугольника = " << S_triangle(h, x) << "." << endl;
+		double h = inizializeDouble();
+		double x = inizializeDouble();
+		cout << "S треугольника = " << computeSquareTriangle(h, x) << "." << endl;
 	}
 	else if (chose == 3)
 	{
 		cout << "Введите значение радиуса круга:\n";
-		double r = err_check();
-		cout << "S круга = " << S_round(r) << "." << endl;
+		double r = inizializeDouble();
+		cout << "S круга = " << computeSquareCircle(r) << "." << endl;
 	}
 	else
 	{
@@ -199,7 +199,7 @@ void f5() {
 }
 
 void f6() {
-
+	
 }
 
 void f7() {
@@ -207,7 +207,59 @@ void f7() {
 }
 
 void f8() {
+	float productsArray[3][4] = { {5, 2, 0, 10}, {3, 5, 2, 5}, {20, 0, 0, 0} };
+	float profitsArray[3][2] = { 0 };
+	float pricesArray[4][2] = { {1.2, 0.5}, {2.8, 0.4}, {5, 1}, {2, 1.5} };
+	cout << endl << "Матрица C = A * B:\n\n";
+	for (size_t i = 0; i < 3; i++) {
+		for (size_t j = 0; j < 4; j++) {
+			// i - index of seller
+			// j - index of product
+			// 0 - index of price
+			// 1 - index of commision
+			profitsArray[i][0] += productsArray[i][j] * pricesArray[j][0];
+			profitsArray[i][1] += productsArray[i][j] * pricesArray[j][1];
+		}
+		cout << profitsArray[i][0] << "\t" << profitsArray[i][1] << endl;
+	}
 
+	float sumProfit = 0, sumCommision = 0, maxProfitSeller, maxCommissionSeller, minProfitSeller, minCommissionSeller;
+
+	float minProfit = numeric_limits<float>::max(), minCommision = minProfit, maxProfit = numeric_limits<float>::lowest(), maxCommision = maxProfit;
+
+	for (size_t i = 0; i < 3; i++) {
+
+		for (size_t k = 0; k < 2; k++) {
+
+			if (k == 0) {
+
+				if (profitsArray[i][k] > maxProfit) {
+					maxProfit = profitsArray[i][k];
+					maxProfitSeller = i + 1;
+				}
+				if (profitsArray[i][k] < minProfit) {
+					minProfit = profitsArray[i][k];
+					minProfitSeller = i + 1;
+				}
+				sumProfit += profitsArray[i][k];
+			}
+			else {
+				if (profitsArray[i][k] > maxCommision) {
+					maxCommision = profitsArray[i][k];
+					maxCommissionSeller = i + 1;
+				}
+				if (profitsArray[i][k] < minCommision) {
+					minCommision = profitsArray[i][k];
+					minCommissionSeller = i + 1;
+				}
+				sumCommision += profitsArray[i][k];
+			}
+		}
+	}
+	cout << "\nОтветы на вопросы задачи:\n1)" << maxProfitSeller << "й продавец выручил больше всего денег с продаж, " << minProfitSeller << "й продавец выручил меньше всего денег с продаж.\n";
+	cout << "2)" << maxCommissionSeller << "й продавец получил наибольшие коммисионные с продаж, " << minCommissionSeller << "й продавец получил наименьшие коммисионные с продаж.\n";
+	cout << "3)Общая сумма денег, вырученных за проданные товары, равна " << sumProfit << ".\n4)Общая сумма коммиссионых, полученных продавцами, равна " << sumCommision << ".\n";
+	cout << "5)Общая сумма денег, прошедших через руки продавцов, равна " << sumProfit + sumCommision << ".\n\n";
 }
 
 void f9() {
@@ -216,9 +268,9 @@ void f9() {
 	cout << "Введите число:\n";
 	cin >> number;
 	cout << "Введите изначальную систему счисления:\n";
-	ss1 = err_check_int();
+	ss1 = inizializeInteger();
 	cout << "Введите конечную систему счисления:\n";
-	ss2 = err_check_int();
+	ss2 = inizializeInteger();
 	vector <char> alphabet; // inicialization of not-fixed len vector
 	for (size_t i = 65; i < 65 + 26; i++) 
 	{
@@ -257,7 +309,7 @@ int main() {
 	setlocale(LC_ALL, "Russian");
 	cout << "Номер задачи не превышает 9.\n" << "Чтобы закончить работу, введите 0.\n";
 	cout << "Введите номер задачи: \n";
-	counter = err_check_int();
+	counter = inizializeInteger();
 	while (counter > 0)
 	{
 		switch (counter)
@@ -294,6 +346,6 @@ int main() {
 			break;
 		}
 		cout << "Введите номер следующей задачи: \n";
-		cin >> counter;
+		counter = inizializeInteger();;
 	}
 }
